ef16c65a2ed9d8cb9e13e7e82dd7994f4cc292a6
shivam sharma <ss0333218@gmail.com>
2025-08-11 09:37:57 +0530

    Updated ChatUi.jsx

--- src/components/ChatUi.jsx
+++ src/components/ChatUi.jsx
@@ -7,9 +7,10 @@ const ChatUi = ({setUserText ,userText}) => {
 <div className='absolute w-full md:w-2/3 h-[71vh] overflow-y-auto left-1/2 -translate-x-1/2 top-0 flex flex-col gap-1'>
 {
   userText.map(ele => (
-  <div key={ele.id} className={ele.sender === 'user' ? 'text-right' : 'text-left'}>
+  <div key={ele.id} className={ele.sender === 'user' ? 'text-right ' : 'text-left'}>
     <p className={ele.sender === 'user'?'inline-block px-3 py-2 text-white bg-black rounded-full text-[13px] md:text-2xl':'inline-block text-[13px] md:text-2xl px-3 py-2 text-black'}>
       {typeof ele.text === 'string' ? ele.text : JSON.stringify(ele.text)}
+    <small className={ele.sender === 'user' ? 'text-white  block text-[8px]' : 'text-black block text-[8px]'}>{ele.timestamp}</small>
     </p>
   </div>
 ))
